87e4bb52943ae4cc3f2ebd63a5fc115c7dc80c4c
Shunichi Shinohara <shino@shiguredo.jp>
2019-11-29 14:18:22 +0900

    file と行パース

---
+++ src/kvconf_file.erl
@@ -0,0 +1,40 @@
+-module(kvconf_file).
+
+-export([open/1]).
+
+-spec open(binary()) -> {ok, map()} | {error, {atom(), any(), non_neg_integer()}.
+open(Path) ->
+    case file:open(Path, [binary, raw]) of
+        {error, Reason} ->
+            {open_error, Reason, 0};
+        {ok, File} ->
+            parse_lines(File, #{}, 1)
+    ok.
+
+parse_lines(File, Configurations, LineNumber) ->
+    case file:read_line(File) of
+        eof ->
+            {ok, Configurations};
+        {error, Reason} ->
+            {error, {read_line_error, Reason, LineNumber}};
+        {ok, [?# | _]} ->
+            parse_lines(File, Configurations, LineNumber + 1);
+        {ok, Line} ->
+            case re:run(Line, <<" *([^ ]*) *= *([^ ]*) *">>, [{capture, all, binary}]) of
+                nomatch ->
+                    case re:run(Line, <<"^ *$">>) of
+                        %% 空白だけの行はスキップする
+                        {match, _} ->
+                            parse_lines(File, Configurations, LineNumber + 1);
+                        _ ->
+                            {error, {invalid_format, Line, LineNumber}}
+                    end;
+                {match, [_, Key, Value]} ->
+                    case maps:is_key(Key, Value) of
+                        true ->
+                            {error, {duplicated_key, Key, LineNumber}};
+                        false ->
+                            parse_lines(File, Configurations#{Key => Value}, LineNumber + 1)
+                    end
+            end
+    end.
